87bf9b2d8126cc1203d0cae12b12e16a664434de
dougieduk <slakingex@gmail.com>
2022-02-06 23:19:47 +0900

    day23_in_progress

---
+++ day23_the_turtle_crossing_capstone/player.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("white")
+        self.penup()
+        self.left(90)
+        self.setpos(STARTING_POSITION[0], STARTING_POSITION[1])
+
+    def move_up(self):
+        self.setpos(self.xcor(), self.ycor() + MOVE_DISTANCE)
+
+    def reached_goal(self):
+        if self.ycor() >= 280:
+            self.setpos(STARTING_POSITION[0], STARTING_POSITION[1])
+            return True
---
+++ day23_the_turtle_crossing_capstone/scoreboard.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.level = 0
+        self.update_level()
+
+    def failure(self):
+        self.level = 0
+        self.setpos(0, 0)
+        self.write("GAME OVER", align="center", font=("Verdana", 20, "normal"))
+
+    def success(self):
+        self.clear()
+        self.level += 1
+        self.update_level()
+
+    def update_level(self):
+        self.setpos(-280, 280)
+        self.write(f"Level : {self.level}", align="left", font=("Verdana", 10, "normal"))
