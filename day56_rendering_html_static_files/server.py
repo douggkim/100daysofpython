4d3d29b3ee3a73847c5927c2c65b18b3622150de
dougieduk <slakingex@gmail.com>
2022-03-07 23:57:11 +0900

    day56_in_progress

--- day56_rendering_html_static_files/server.py
+++ day56_rendering_html_static_files/server.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def hello(name=None):
+    # html files will have to be inside a folder called 'templates'
+    # images for html files will have to be inside a folder called 'static' -> static/photo.img
+    return render_template('index.html')
 
+@app.route("/doug")
+def doug(name=None):
+    return render_template("introduction.html")
 
-if __name__ == "__main":
+if __name__ == "__main__":
     app.run(debug=True)
